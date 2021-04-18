0cf302a0480f2db13a0232915bd567f3059707ab
Marcus Mors <alivezeh@gmail.com>
2021-04-18 02:31:39 -0500

    :sparkles: Javascript
    finally...

--- javascript/app.js
+++ javascript/app.js
@@ -1,15 +1,15 @@
 // Param numbers: array of numbers
 // Returns a sorted array
 function insertion_sort(numbers) {
-	let size = inputArr.length
+	let size = numbers.length
 	for (let i = 1; i < size; i++) {
-		let current = inputArr[i]
+		let current = numbers[i]
 		let j = i - 1
-		while (j >= 0 && current < inputArr[j]) {
-			inputArr[j + 1] = inputArr[j]
+		while (j >= 0 && current < numbers[j]) {
+			numbers[j + 1] = numbers[j]
 			j--
 		}
-		inputArr[j + 1] = current
+		numbers[j + 1] = current
 	}
 	console.log(numbers)
 	return numbers
@@ -19,7 +19,7 @@ function insertion_sort(numbers) {
 // Returns a single integer
 function factorial(number) {
 	let mult = 1
-	for (i = 1; i <= number; i++) {
+	for (let i = 1; i <= number; i++) {
 		mult = mult * i
 	}
 	console.log(mult)
@@ -30,7 +30,6 @@ function factorial(number) {
 // Returns a single integer
 function fibonacci(number) {
 	if (number < 2) {
-		console.log("executed")
 		return number
 	}
 	return fibonacci(number - 1) + fibonacci(number - 2)
@@ -39,41 +38,43 @@ function fibonacci(number) {
 // Param expression: string containing a mathematical expression
 // Returns an integer value
 function evaluation(result) {
-	var chars = result.split("")
-	var n = [],
-		op = [],
-		index = 0,
-		oplast = true
-	n[index] = ""
-	for (var c = 0; c < chars.length; c++) {
-		if (isNaN(parseInt(chars[c])) && chars[c] !== "." && !oplast) {
-			op[index] = chars[c]
-			index++
-			n[index] = ""
-			oplast = true
-		} else {
-			n[index] += chars[c]
-			oplast = false
-		}
-	}
-	result = parseFloat(n[0])
-	for (var o = 0; o < op.length; o++) {
-		var num = parseFloat(n[o + 1])
-		switch (op[o]) {
-			case "+":
-				result = result + num
-				break
-			case "-":
-				result = result - num
-				break
-			case "*":
-				result = result * num
-				break
-			case "/":
-				result = result / num
-				break
-		}
-	}
+	let size = result.size()
+	for(let i = 0; i < result.size())
+	// let chars = result.split("")
+	// let n = []
+	// let op = []
+	// let index = 0
+	// let oplast = true
+	// n[index] = ""
+	// for (let i = 0; i < chars.length; i++) {
+	// 	if (isNaN(parseInt(chars[i])) && chars[i] !== "." && !oplast) {
+	// 		op[index] = chars[i]
+	// 		index++
+	// 		n[index] = ""
+	// 		oplast = true
+	// 	} else {
+	// 		n[index] += chars[i]
+	// 		oplast = false
+	// 	}
+	// }
+	// result = parseFloat(n[0])
+	// for (let o = 0; o < op.length; o++) {
+	// 	let num = parseFloat(n[o + 1])
+	// 	switch (op[o]) {
+	// 		case "+":
+	// 			result = result + num
+	// 			break
+	// 		case "-":
+	// 			result = result - num
+	// 			break
+	// 		case "*":
+	// 			result = result * num
+	// 			break
+	// 		case "/":
+	// 			result = result / num
+	// 			break
+	// 	}
+	// }
 	console.log("")
 	return result
 }
@@ -86,9 +87,4 @@ if (typeof process === "object") {
 		evaluation,
 	}
 }
-module.exports = {
-	insertion_sort,
-	factorial,
-	fibonacci,
-	evaluation,
-}
+export { insertion_sort, factorial, fibonacci, evaluation }
--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,26 +1,45 @@
 //Place your javascript code related with HTML and CSS here
-const { insertion_sort, factorial, fibonacci, evaluation } = require("app.js")
+import * as app from "./app.js"
 
-function do_insertion_sort() {
-	document.getElementById("insertion-sort-form").submit()
-	insertion_sort(numbers.split(","))
-}
-function do_fibonacci() {
-	document.getElementById("fibonacci-form").submit()
-	fibonacci(+number)
-}
-function do_factorial() {
-	document.getElementById("factorial-form").submit()
-	factorial(+number)
-}
-function do_evaluation() {
-	document.getElementById("evaluation-form").submit()
-	evaluation(expression)
-}
+// $("insertion - sort - form").submit(function (event) {
+// 	event.preventDefault()
+// 	const input = $("insertion-sort-input").val()
+// 	alert(input)
+// })
 
-module.exports = {
-	do_insertion_sort,
-	do_fibonacci,
-	do_factorial,
-	do_evaluation,
+function doInsertionSort() {
+	const input = document.getElementById("insertion-sort-input").value
+	const output = app.insertion_sort(input.split(","))
+	event.preventDefault()
+	document.getElementById("insertion-sort-output").innerHTML = output
 }
+document
+	.getElementById("btn-insertion-sort")
+	.addEventListener("click", doInsertionSort)
+function doFactorial() {
+	const input = document.getElementById("factorial-input").value
+	const output = app.factorial(+input)
+	event.preventDefault()
+	document.getElementById("factorial-output").innerHTML = output
+}
+document.getElementById("btn-factorial").addEventListener("click", doFactorial)
+function doFibonacci() {
+	const input = document.getElementById("fibonacci-input").value
+	const output = app.fibonacci(+input)
+	event.preventDefault()
+	document.getElementById("fibonacci-output").innerHTML = output
+}
+document.getElementById("btn-fibonacci").addEventListener("click", doFibonacci)
+function doExpression() {
+	const input = document.getElementById("expression-input").value
+	const output = app.evaluation(input)
+	event.preventDefault()
+	document.getElementById("expression-output").innerHTML = output
+}
+document
+	.getElementById("btn-expression")
+	.addEventListener("click", doExpression)
+
+// factorial - form
+// fibonacci - form
+// evaluation - form
